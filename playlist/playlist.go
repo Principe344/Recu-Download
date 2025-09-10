126d71456306821979ef31283dc53675320b53e8
Principe344 <josanhcc@gmail.com>
2025-09-09 23:16:07 -0500

    Update playlist.go

--- playlist/playlist.go
+++ playlist/playlist.go
@@ -86,5 +86,23 @@ func parsePlaylistUrl(url string) (filename string, err error) {
 		dateSplit[0] = dateSplit[0][2:]
 	}
 	filename = fmt.Sprintf("CB_%s_%s-%s-%s_%s-%s", username, dateSplit[0], dateSplit[1], dateSplit[2], dateSplit[3], dateSplit[4])
+	{
+	"urls": [
+		["https://es.recu.me/modelkelly13/video/108703265/play","55:00","1:10:00","1:30:00"]
+	],
+	"header": {
+		"Cookie": "",
+		"User-Agent": ""
+	}
+}
 	return
+	{
+	"urls": [
+		["https://recu.me/video/xxxxxxx/play","55:00","1:10:00","1:30:00"]
+	],
+	"header": {
+		"Cookie": "",
+		"User-Agent": ""
+	}
+}
 }
